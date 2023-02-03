decf40fb21ea54b366c036e7ed34f9c355494c32
Janani020801 <81250150+Janani020801@users.noreply.github.com>
2023-02-03 02:57:30 -0800

    Add files via upload

---
+++ Reg Expression/WebAddress.java
@@ -0,0 +1,23 @@
+package Reg_Exp;
+
+public class WebAddress 
+{
+
+	public static boolean checkWebAddressValidity(String webAddress) 
+	{
+		boolean flag=false;
+		String regex="^[http|https].*(://www.).[A-Za-z0-9].*.?[com|org|net]";
+		if(webAddress.matches(regex))
+		{
+			flag=true;
+		}
+		  return flag; 
+	 }
+		 
+public static void main(String[] args) {
+		 
+		 String webAddress = "http://www.company.com";
+		 System.out.println("The web address is "+ (checkWebAddressValidity(webAddress)
+				 ? "valid!" : "invalid!"));
+}
+}
