3f57479393b841dc2e6367899669885125f68fa9
Janani020801 <81250150+Janani020801@users.noreply.github.com>
2023-02-14 21:06:15 -0800

    Add files via upload

---
+++ Assignment 3 Book Management System/BookSearch.java
@@ -0,0 +1,50 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.SQLException;
+import java.sql.Statement;
+public class BookSearch {
+
+	public static void main(String[] args) throws Exception
+	{
+		Class.forName("com.mysql.cj.jdbc.Driver");
+
+	      // variables
+	      final String url = "jdbc:mysql:///sys";
+	      final String user = "root";
+	      final String password = "Janani@28";
+
+	      // establish the connection
+	      Connection con = DriverManager.getConnection(url, user, password);
+	      // create JDBC statement object
+	      Statement st = con.createStatement();
+
+	      // prepare SQL query
+	      String query = "SELECT * FROM bookDetails";
+
+	      // send and execute SQL query in Database software
+	      ResultSet rs = st.executeQuery(query);
+
+	      // process the ResultSet object
+	      boolean flag = false;
+	      while (rs.next()) {
+	         flag = true;
+	         System.out.println(rs.getInt(1) + " " + rs.getString(2));
+	      }
+
+	      if (flag == true) {
+	         System.out.println("\nRecords retrieved and displayed");
+	      } else {
+	         System.out.println("Record not found");
+	      }
+	   // close JDBC objects
+	      rs.close();
+	      st.close();
+	      con.close();
+
+	}
+
+}
